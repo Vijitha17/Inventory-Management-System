b2dcdfbe0b99ed25f5ce70cfcd39270a3ebfe077
Vijitha M <vijithamoorthi85@gmail.com.com>
2025-03-24 13:35:08 +0530

    Register User

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,32 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const userRoute = require("./routes/userRoute");
+const errorHandler = require("./middleWare/errorMiddleware");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(cors()); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes Middleware
+app.use("/api/users", userRoute);
+
+//Routes 
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+//Error Middleware
+app.use(errorHandler);
+
+
+//connect to DB and start server
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {

d818213aa614b4160b27121766cf4aacff3749b6
Vijitha M <vijithamoorthi85@gmail.com.com>
2025-03-25 12:36:44 +0530

    Login Module

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const userRoute = require("./routes/userRoute");
 const errorHandler = require("./middleWare/errorMiddleware");
+const cookieParser = require("cookie-parser");
+
 
 const app = express();
 
@@ -13,6 +15,7 @@ const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(cors()); 
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
